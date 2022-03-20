617f1130316425b814b0347a4c4d1c231033a5e5
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-03-20 18:49:30 +0530

    Update track_status.py

--- track_status.py
+++ track_status.py
@@ -63,7 +63,7 @@ def main():
     
     
         googlesheeturl="https://docs.google.com/spreadsheets/d/17EW9CAOOwmefEP4toLAubQhj7q-NgsjLRAlzCeKvIIs/edit#gid=1757221736"
-        creds=ServiceAccountCredentials.from_json_keyfile_name(KEY_1,scope)
+        creds=ServiceAccountCredentials.from_json_keyfile_name(st.file_uploader("Upload Json Key File"),scope)
         # st.write(creds.get_access_token())
         client=gspread.authorize(creds)
         sheet=client.open_by_url(googlesheeturl)
