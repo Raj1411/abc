ec497ccd9bbdb2f8490ca724c31350c5f943a0f1
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-03-20 17:20:48 +0530

    Add files via upload

---
+++ track_status.py
@@ -0,0 +1,170 @@
+
+from oauth2client.client import AUTHORIZED_USER
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import date
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pretty_html_table import build_table
+import time
+import pyautogui
+from PIL import Image
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+# from decouple import config
+from settings import KEY_1, SHEET_LINK
+
+
+# print(KEY_1)
+
+
+menu_touchup="""
+<style>
+#MainMenu {
+    visibility: hidden;
+}
+footer:before {
+    content: 'Developer [Rajinder Singh]';    
+    display: block;
+    max-width: 1000px;
+    margin:0px auto;
+    width: 100%;
+    height: 30px;
+    position: center;
+    text-align: center;
+    padding: 5px;  
+    top: 3px;
+    background: #666;
+    color: white;
+
+</style>
+"""
+
+
+
+
+scope=['https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/spreadsheets']
+
+
+
+def main():
+    # img=Image.open("D:\\Office\\Python\\Arvind sir\\logo.png")
+    # st.image(img,width=200)
+    st.markdown('<h1 style="text-align: center;">Shipment Status Tracker</h1>', unsafe_allow_html=True) 
+    st.markdown('''---------------------------------------------------------------------------------------------------------''')
+    st.markdown(menu_touchup,unsafe_allow_html=True)
+
+    btn1=st.button('Track Shipment Status')
+
+    if btn1:
+
+    
+    
+        googlesheeturl=SHEET_LINK
+        creds=ServiceAccountCredentials.from_json_keyfile_name(KEY_1,scope)
+        # st.write(creds.get_access_token())
+        client=gspread.authorize(creds)
+        sheet=client.open_by_url(googlesheeturl)
+        main_worksheet=sheet.worksheet('Sheet3')
+        pasted_value_worksheet=sheet.worksheet('Sheet3')
+        # worksheet=sheet.get_worksheet(0)
+        # sheet_runs = sheet.get_worksheet(0)
+
+
+        df=main_worksheet.get_all_values()
+        df=pd.DataFrame(df,columns=['Portal','Po No','Appointment_Date','FC','Courier','Status'])
+
+        df.drop(df.index[0],inplace=True)
+        df.drop(df.index[2],inplace=True)
+        df.drop(df.index[2],inplace=True)
+        df=df[df['Status']!='Delivered']
+        df=df[df['Status']!='Cancel']
+        df=df[df['Appointment_Date']!='Pending']
+        df['Appointment_Date']=pd.to_datetime(df['Appointment_Date'])
+        df=df[df['Appointment_Date']<pd.to_datetime(date.today())]
+        # st.write(df)
+
+        if df.empty:
+            st.write('No Shipment to track')
+        else:
+            st.write('Total Shipment to track: ',len(df))
+            # st.table(df)
+            st.write('https://api.whatsapp.com/send?phone=919811648522&text=Hi%20Mam%20The%20Status%20has%20not%20been%20updated%20for%20these%20shipments%20PO%20No%20')
+            # st.table(df)
+            # st.plotly_chart(ff.create_table(df))
+            fig=go.Figure(data=[go.Table(
+                columnorder=[1,2,3,4,5,6],
+                columnwidth=[250,250,250,250,250,200],
+                header=dict(values=['Portal','Po No','Appointment_Date','FC','Courier','Status'],height=30,align='center',font=dict(size=15,color='white'),fill_color='#3D9970'),
+                cells=dict(values=[df['Portal'],df['Po No'],df['Appointment_Date'].dt.strftime('%d-%b-%Y'),df['FC'],df['Courier'],df['Status']]))])
+            fig.update_layout(width=1000,height=1500)
+            st.plotly_chart(fig)
+
+            # st.write(df)
+            # st.write(df['PO_No'])
+            # st.write(df['Courier'])
+            # st.write(df['FC'])
+            # st.write(df['Appointment_Date'])
+            # st.write(df['Tracking_id'])
+            # st.write(df['Status'])
+            # st.write(df['Portal'])
+
+            # st.write(df.PO_No)
+            # st.write(df.Courier)
+            # st.write(df.FC)
+            # st.write(df.Appointment_Date)
+            # st.write(df.Tracking_id)
+            # st.write(df.Status)
+            # st.write(df.Portal)
+
+            # st.write(df.PO_No)
+            # st.write(df.Courier)
+            # st.write(df.FC)
+            # st.write(df.Appointment_Date)
+            # st.write(df.Tracking_id)
+            # st.write(df.Status)
+            # st.write(df.Portal)
+
+            # st.write(df.PO_No)
+            # st.write(df.Courier)
+            # st.write(df.FC)
+            # st.write(df.Appointment_Date)
+            # st.write(df.Tracking_id)
+            # st.write(df.Status)
+            # st.write(df.Portal)
+
+            # st.write(df.PO_No)
+            # st.write(df.Courier)
+            # st.write(df.FC)
+            # st.write(df.Appointment_Date)
+            # st.write(df.Tracking_id)
+            # st.write(df.Status)
+            # st.write(df.Portal)
+
+            # st.write(df.PO_No)
+            # st.write(df.Courier)
+            # st
+
+
+            # time.sleep(4)
+            # ss=pyautogui.screenshot()
+            # img=np.array(ss)
+            # img=Image.fromarray(img)
+            # w,h=img.size
+            # st.write(w,h)
+            # img=img.crop((400,600,w-20,h-80))
+            # st.image(img,width=1500)
+
+
+            # for i in range(len(df)):
+            #     st.write('PO No: ', df.iloc[i]['PO No']
+            #     ,'\nCourier: ',df.iloc[i]['Courier']
+            #     ,'\nFC: ',df.iloc[i]['FC']
+            #     ,'\nAppointment Date: ',df.iloc[i]['Appointment_Date']
+            #     ,'\nPortal: ',df.iloc[i]['Portal'])
+
+
+if __name__ == '__main__':
+    main()
